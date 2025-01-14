bbdd4ccb098f2babffa887ff2fa608ba3702e315
Kayode <dadavictory2000@gmail.com>
2025-01-14 16:50:58 +0100

    changed project section background

--- src/components/sections/projects/Projects.jsx
+++ src/components/sections/projects/Projects.jsx
@@ -1,32 +1,29 @@
-import { Button } from "@mui/material";
-
 import { projects } from "@/utils/constants";
-import { ArrowRight, Github, Link as LinkIcon } from "lucide-react";
-import ShortenText from "@/utils/Shortentext";
 import Link from "next/link";
 import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
   return (
     <section
-      className="w-full py-24 px-5 relative text-black dark:text-white"
+      className="w-full py-24 px-5 relative text-black dark:text-white bg-slate-200 dark:bg-stone-900"
       id="projects"
     >
-      <img
+      <div className="main z-10"></div>
+      {/* <img
         src="/images/bg2.png"
         className="z-0 absolute w-full h-full top-0 left-0"
-      />
-      <section className="relative mx-auto flex flex-col justify-center items-center gap-3">
+      /> */}
+      <section className="relative mx-auto flex flex-col justify-center items-center gap-3 z-20">
         <div
           // initial={{ opacity: 0, scale: 0.5 }}
           // animate={{ opacity: 1, scale: 1 }}
           // transition={{ duration: 1.5 }}
           className="text-center flex flex-col justify-center items-center"
         >
-          <h1 className="header font-semibold sm:font-medium sm:text-3xl tracking-wide capitalize text-white">
+          <h1 className="header font-semibold sm:font-medium sm:text-3xl tracking-wide capitalize ">
             my projects
           </h1>
-          <h1 className="text-center sm:max-w-[50%] text-gray-200 body_text tracking-wide font-light my-4">
+          <h1 className="text-center sm:max-w-[50%]  body_text tracking-wide font-light my-4">
             Explore my latest work, blending creativity and code for functional
             responsive website with clean design and seeamless user experience
           </h1>
@@ -46,7 +43,7 @@ const Projects = () => {
             }
           })}
         </div>
-        <Link href={'/projects'} className="bg-gray-200 text-blue-700 px-6 py-3 rounded-lg font-semibold shadow-md hover:bg-blue-100 transition duration-200 mt-10">
+        <Link href={'/projects'} className="bg-blue-200 text-gray-700 px-6 py-3 rounded font-semibold shadow hover:bg-blue-100 transition duration-200 mt-10">
           View All Projects
         </Link>
       </section>
